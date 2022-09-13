4f903d181c3e70a9864d527987acfab65df63909
MD EMRUL EMRAN <emrulhasan@gmail.com>
2022-09-13 14:25:52 -0400

    Create CutPaste.cs

---
+++ CutPaste.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Threading;
+using System.Xml.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumClass
+{
+    public class CutAndPasteToAnotherTextField
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Manage().Cookies.DeleteAllCookies();
+            driver.Navigate().GoToUrl("https://demo.guru99.com/test/newtours/register.php");
+
+            IWebElement fName = driver.FindElement(By.Name("firstName"));
+            IWebElement lName = driver.FindElement(By.Name("lastName"));
+
+            Actions act = new Actions(driver);
+            act.SendKeys(fName, "Jamel").Build().Perform();
+
+            act
+                .KeyDown(Keys.Shift)
+                .SendKeys(Keys.ArrowLeft)
+                .SendKeys(Keys.ArrowLeft)
+                .SendKeys(Keys.ArrowLeft)
+                .SendKeys(Keys.ArrowLeft)
+                .SendKeys(Keys.ArrowLeft)
+                .SendKeys(Keys.ArrowLeft)
+                .KeyUp(Keys.Shift)
+                .KeyDown(Keys.Control)
+                .SendKeys("x")
+                .SendKeys(lName, "v")
+                .KeyUp(Keys.Control).Build().Perform();
+        }
+    }
+}
